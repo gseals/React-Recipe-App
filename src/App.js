12638887976ec2bbcb4df57be36be4b148b5a6b4
Gabriel Seals <sealsgj@gmail.com>
2020-09-10 19:42:32 -0500

    beginning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import apiStuff from './helpers/apiFish.json';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'apiStuff.apiID'
+  const APP_KEY = 'apiStuff.apiK'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
