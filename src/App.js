9a7b518182ee60fb6646c6ecbd05b2d00ba21d89
Gabriel Seals <sealsgj@gmail.com>
2020-09-15 20:41:55 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import apiStuff from './helpers/apiFish.json';
 
 const App = () => {
+  const APP_ID = apiStuff.apiID;
+  const APP_KEY = apiStuff.apiK;
 
-  const APP_ID = 'apiStuff.apiID'
-  const APP_KEY = 'apiStuff.apiK'
+  useEffect(() => {
+    
+  }, []);
+
+  const GetRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
